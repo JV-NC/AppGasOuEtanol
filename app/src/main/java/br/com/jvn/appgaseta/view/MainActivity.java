017c1edf70585dc48d0134562ed6ec1b5204775f
João Vitor <joaovnevescosta@hotmail.com>
2024-05-23 01:05:12 -0300

    refactor lblResultado

--- app/src/main/java/br/com/jvn/appgaseta/view/MainActivity.java
+++ app/src/main/java/br/com/jvn/appgaseta/view/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import br.com.jvn.appgaseta.R;
@@ -15,6 +16,7 @@ import br.com.jvn.appgaseta.apoio.UtilGasEta;
 public class MainActivity extends AppCompatActivity {
     EditText tfValorGas;
     EditText tfValorEta;
+    TextView lblResultado;
     Button btnCalcular;
     Button btnLimpar;
     Button btnSalvar;
@@ -32,11 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private void setLayout(){
         tfValorGas = findViewById(R.id.tfValorGas);
         tfValorEta = findViewById(R.id.tfValorEta);
+        lblResultado = findViewById(R.id.lblResultado);
         btnCalcular = findViewById(R.id.btnCalcular);
         btnLimpar = findViewById(R.id.btnLimpar);
         btnSalvar = findViewById(R.id.btnSalvar);
         btnFinalizar = findViewById(R.id.btnFinalizar);
-        btnSalvar.setEnabled(false);
     }
 
     private void setButtons(){
@@ -56,12 +58,13 @@ public class MainActivity extends AppCompatActivity {
                     verify = false;
                 }
                 if(verify){
-                    Toast.makeText(MainActivity.this, UtilGasEta.calcularMelhorOpcao(Double.parseDouble(tfValorGas.getText().toString()),Double.parseDouble(tfValorEta.getText().toString()),UtilGasEta.PADRAO_70),Toast.LENGTH_SHORT).show();
+                    lblResultado.setText(UtilGasEta.calcularMelhorOpcao(Double.parseDouble(tfValorGas.getText().toString()),Double.parseDouble(tfValorEta.getText().toString()),UtilGasEta.PADRAO_70));
                     btnSalvar.setEnabled(true);
                 }
                 else{
                     Toast.makeText(MainActivity.this,"Falha ao Calcular!",Toast.LENGTH_SHORT).show();
                     btnSalvar.setEnabled(false);
+                    lblResultado.setText("");
                 }
             }
         });
@@ -70,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tfValorGas.setText("");
                 tfValorEta.setText("");
+                lblResultado.setText("");
+
+                btnSalvar.setEnabled(false);
             }
         });
         btnFinalizar.setOnClickListener(new View.OnClickListener() {
