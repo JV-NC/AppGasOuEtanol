2af02b1056fa8064b52dcbbb0f0064bca2275a8b
João Vitor <joaovnevescosta@hotmail.com>
2024-06-03 20:38:52 -0300

    refactor Layout recycler

--- app/src/main/java/br/com/jvn/appgaseta/database/GasEtaDBCore.java
+++ app/src/main/java/br/com/jvn/appgaseta/database/GasEtaDBCore.java
@@ -5,8 +5,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import androidx.annotation.Nullable;
-
 import java.util.UUID;
 
 public class GasEtaDBCore extends SQLiteOpenHelper {
--- app/src/main/java/br/com/jvn/appgaseta/view/RecyclerActivity.java
+++ app/src/main/java/br/com/jvn/appgaseta/view/RecyclerActivity.java
@@ -1,10 +1,14 @@
 package br.com.jvn.appgaseta.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,10 +22,10 @@ import br.com.jvn.appgaseta.model.Combustivel;
 import br.com.jvn.appgaseta.model.CombustivelAdapter;
 
 public class RecyclerActivity extends AppCompatActivity {
-ArrayList<Combustivel> list;
-RecyclerView rv;
-FloatingActionButton fabSair;
-CombustivelAdapter combustivelAdapter;
+    Toolbar toolbar;
+    ArrayList<Combustivel> list;
+    RecyclerView rv;
+    CombustivelAdapter combustivelAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,18 +35,30 @@ CombustivelAdapter combustivelAdapter;
         if(list.size()==0){
             Log.e("List Parse","Lista Vazia");
         }
+        toolbar = findViewById(R.id.toolbarList);
+        setSupportActionBar(toolbar);
+
 
         rv = findViewById(R.id.recyclerGasEta);
-        fabSair = findViewById(R.id.fabSair);
 
         combustivelAdapter = new CombustivelAdapter(list);
 
         rv.setAdapter(combustivelAdapter);
-        fabSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_recycler_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id==R.id.itemRecyclerClose){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
